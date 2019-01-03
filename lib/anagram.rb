31fdfbb324fbe0b420e03452b194a4f9219531a6
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-03 20:05:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
   end
 
   def match(array)
-    array.select {|x| x.split("").sort == @word.split("").sort}
+    array.select {|x| x.split(//).sort == @word.split(//).sort}
   end
 
 end
